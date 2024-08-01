15a2556eafef5724d01fbb047db72faa1373dbf8
niikun <123465566+niikun@users.noreply.github.com>
2024-08-01 14:15:42 +0000

    first commint

---
+++ hello/target/debug/deps/hello-12cd205facd0b75b.d
@@ -0,0 +1,5 @@
+/workspaces/intro-rust/hello/target/debug/deps/hello-12cd205facd0b75b: src/main.rs
+
+/workspaces/intro-rust/hello/target/debug/deps/hello-12cd205facd0b75b.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/workspaces/intro-rust/hello/target/debug/hello: /workspaces/intro-rust/hello/src/main.rs
